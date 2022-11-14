544cc93e1cd0aac193324c2fd6a6112541f90e21
annaweistn <101801514+annaweistn@users.noreply.github.com>
2022-11-14 17:56:59 +0100

    Schriftgröße optimiert

--- I_am_not_okay_but_it_is_okay_2022_11_14_16_50_47/sketch.js
+++ I_am_not_okay_but_it_is_okay_2022_11_14_16_50_47/sketch.js
@@ -1,29 +1,26 @@
-
 let myText;
 let myText1;
 
-
-
 function setup() {
-  createCanvas(640, 360); 
+  createCanvas(640, 360);
   let initialProps = {
-    text:'I am not okay ',
-    color: 'white',
-    size: 30,
-    position: { x: 100, y: 120 }
+    text: "I am not okay ",
+    color: "white",
+    size: 20,
+    position: { x: 100, y: 120 },
   };
   let initialProps1 = {
-    text:'But it is okay',
-    color: 'white',
-    size: 30,
-    position: { x: 50, y: 150 }
+    text: "But it is okay",
+    color: "white",
+    size: 40,
+    position: { x: 50, y: 150 },
   };
   myText = createText(initialProps);
   myText1 = createText(initialProps1);
 }
 
 function draw() {
-  background('black');
+  background("black");
 }
 
 function mousePressed() {
@@ -33,32 +30,32 @@ function mousePressed() {
 
 function basicPromises() {
   myText
-    .animate({ color: 'red', duration: 3})
+    .animate({ color: "red", duration: 3 })
     .then(() => {
       return myText.animate({
-        color: 'white',
+        color: "white",
         size: 20,
-        duration: 3
+        duration: 3,
       });
     })
     .then(() => {
       return myText.animate({
         size: 30,
-        duration: 3
+        duration: 3,
       });
     })
-    .catch(e => console.error(`Oops: ${e}`));
+    .catch((e) => console.error(`Oops: ${e}`));
 }
 
 function friendlyPromises() {
   myText.animate([
-    { color: 'red', duration: 3 },
-    { color: 'white', size: 20, duration: 3 },
+    { color: "red", duration: 3 },
+    { color: "white", size: 20, duration: 3 },
     { size: 30, duration: 3 },
   ]);
   myText1.animate([
-    { color: 'green', duration: 2 },
-    { color: 'white', size: 20, duration: 2 },
+    { color: "green", duration: 2 },
+    { color: "white", size: 20, duration: 2 },
     { size: 30, duration: 3 },
   ]);
 }
